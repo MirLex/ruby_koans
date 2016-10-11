af5ec862164daf40e2b6726357c26f8701027b00
Mirshavka Aleksandr <mirshavka.alexander@gmail.com>
2016-10-11 17:57:53 +0300

    /PR complete about_triangle_project_2.rb

--- triangle.rb
+++ triangle.rb
@@ -14,6 +14,16 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  if [a,b,c].min <= 0
+      raise TriangleError, "triangle side length must be a positive value"
+  end
+
+  [a,b,c].permutation do |sides|
+    if sides[0] + sides[1] <= sides[2]
+      raise TriangleError, "One side of the triangle more than the sum of the other two sides"
+    end
+  end
+
   if a == b && a == c 
     :equilateral
   elsif a == b || b == c || a == c
